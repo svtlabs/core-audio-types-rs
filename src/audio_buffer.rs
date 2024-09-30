f6419b371435c5bc1502036794b34093f14fee2e
Per Johansson <mail@perjohansson.net>
2024-09-30 15:00:29 +0200

    make buffer pointers generic

--- src/audio_buffer.rs
+++ src/audio_buffer.rs
@@ -1,15 +1,15 @@
-use std::{os::raw::c_void, ptr::NonNull};
+use std::ptr::NonNull;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct AudioBuffer {
+pub struct AudioBuffer<T = u8> {
     pub number_channels: u32,
     pub data_byte_size: u32,
-    pub data: NonNull<c_void>,
+    pub data: NonNull<T>,
 }
 
-impl AudioBuffer {
-    pub fn new(number_channels: u32, data_byte_size: u32, data: NonNull<c_void>) -> Self {
+impl<T> AudioBuffer<T> {
+    pub fn new(number_channels: u32, data_byte_size: u32, data: NonNull<T>) -> Self {
         Self {
             number_channels,
             data_byte_size,
--- src/audio_value_translation.rs
+++ src/audio_value_translation.rs
@@ -2,17 +2,17 @@ use std::{ffi::c_void, ptr::NonNull};
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct AudioValueTranslation {
-    pub input_data: NonNull<c_void>,
-    pub output_data: NonNull<c_void>,
+pub struct AudioValueTranslation<TInput = c_void, TOutput = c_void> {
+    pub input_data: NonNull<TInput>,
+    pub output_data: NonNull<TOutput>,
     pub input_data_size: u32,
     pub output_data_size: u32,
 }
 
-impl AudioValueTranslation {
+impl<TInput, TOutput> AudioValueTranslation<TInput, TOutput> {
     pub fn new(
-        input_data: NonNull<c_void>,
-        output_data: NonNull<c_void>,
+        input_data: NonNull<TInput>,
+        output_data: NonNull<TOutput>,
         input_data_size: u32,
         output_data_size: u32,
     ) -> Self {
